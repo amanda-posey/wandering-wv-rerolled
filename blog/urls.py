4bd271c57094483d52a66dacca640b098adaebf5
amanda=posey <amanda.posey@live.com>
2021-06-25 16:44:33 -0400

    initial commit

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.starting_page, name='starting-page'),
+    path('posts', views.posts, name='posts-page'),
+    path('posts/<slug>', views.post_detail, name='detail-page')
+]
