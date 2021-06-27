22baf4286b5767b3a56b156abe380dc06640781c
amanda=posey <amanda.posey@live.com>
2021-06-27 15:46:23 -0400

    styling looks good. still no db

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.starting_page, name='starting-page'),
     path('posts', views.posts, name='posts-page'),
-    path('posts/<slug>', views.post_detail, name='detail-page')
+    path('posts/<slug:slug>', views.post_detail, name='detail-page')
 ]
